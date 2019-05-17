49fd96bd5214cb6cd14d8a4be11ccb523afa7671
Jethro Wong <jethro.yuetou.wong@gmail.com>
2019-05-17 14:11:24 -0400

    Added super statement and formatted

--- city.java
+++ city.java
@@ -1,28 +1,27 @@
-public class city extends settlement{
+public class city extends settlement
+{
 
-	// Properties 
+	// Properties
 	/** is the number of points the player has */
-	int intVP;
+	int intVP = 2;
 	/** is the number that player is assigned */
-	int intPlayer;
-	/** tile 1 */ 
-	int intT1;
-	/** tile 2 */
-	int intT2;
-	/** tile 3*/
-	int intT3;
+
 	// Methods
-	/** draws a city if there are 3 tiles surrounding the player */	
-	public void draw(int intPlayer, int intT1, int intT2, int intT3){
-	} 
+	/** draws a city if there are 3 tiles surrounding the player */
+	public void draw (int intPlayer, int intT1, int intT2, int intT3)
+	{
+	}
+
 	/** draws a city if there are 2 tiles surrounding the player */
-	public void draw(int intPlayer, int intT1, int intT2){
+	public void draw (int intPlayer, int intT1, int intT2)
+	{
 	}
 
 	// Constructor
-	/** contructs a city*/
-	public city(int intPlayer, int intT1, int intT2, int intT3){
+	/** contructs a city */
+	public city(int intPlayer, int intT1, int intT2, int intT3)
+	{
+		super(intPlayer, intT1, intT2, intT3);
 	}
 
 }
-
--- settlement.java
+++ settlement.java
@@ -1,28 +1,31 @@
-public class settlement{
-	// Properties 
+public class settlement
+{
+	// Properties
 	/** is the number of points the player has */
 	int intVP;
 	/** is the number that player is assigned */
 	int intPlayer;
-	/** tile 1 */ 
+	/** tile 1 */
 	int intT1;
 	/** tile 2 */
 	int intT2;
-	/** tile 3*/
+	/** tile 3 */
 	int intT3;
 
 	// Methods
-	/** draws a settlement if there are 3 tiles surrounding the player */	
-	public void draw(int intPlayer, int intT1, int intT2, int intT3){
-	} 
+	/** draws a settlement if there are 3 tiles surrounding the player */
+	public void draw (int intPlayer, int intT1, int intT2, int intT3)
+	{
+	}
+
 	/** draws a settlement if there are 2 tiles surrounding the player */
-	public void draw(int intPlayer, int intT1, int intT2){
+	public void draw (int intPlayer, int intT1, int intT2)
+	{
 	}
 
 	// Constructor
-	/** contructs a settlement*/
-	public settlement(int intPlayer, int intT1, int intT2, int intT3){
+	/** contructs a settlement */
+	public settlement(int intPlayer, int intT1, int intT2, int intT3)
+	{
 	}
-
-
 }
