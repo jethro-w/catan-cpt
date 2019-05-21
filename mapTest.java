4d73a5b8123f1950d100f5097728e8efd78ffd51
Jethro Wong <jethro.yuetou.wong@gmail.com>
2019-05-21 12:59:05 -0400

    Changed name of csv

--- mapTest.java
+++ mapTest.java
@@ -2,11 +2,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+// Purely for the settlements (ie. the intersections)
 public class mapTest
 {
 	public static void main (String[] args) throws IOException
 	{
-		BufferedReader map = new BufferedReader(new FileReader("catan-map.csv"));
+		BufferedReader map = new BufferedReader(new FileReader("catan-settlements-map.csv"));
 		String[] strMapLine = new String[12];
 		String[][] strMap = new String [12][11];
 		int intCount;
