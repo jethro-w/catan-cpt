4f8c9b289399dd147f2fe4fa55bad487fb124e89
jasonchan239 <50674864+jasonchan239@users.noreply.github.com>
2019-05-28 09:32:47 -0400

    Revert "Merge branch 'master' of https://github.com/jethro-w/catan-cpt"
    This reverts commit 0454bc6409a6f43b739852c6420154c4ac8af6a1, reversing
    changes made to 21c2131d47b75384178e1448f8b5ba2bd5de7f29.

--- AnimationPanel.java
+++ AnimationPanel.java
@@ -3,48 +3,23 @@ import javax.swing.*;
 import java.io.*;
 import java.awt.image.*;
 import javax.imageio.*;
+import java.io.*;
 
 public class AnimationPanel extends JPanel
 {
 	// Properties
-	int intOreX;
-	int intOreY;
-	BufferedImage image = null;
-	boolean printTile;
 
 	// Methods
 	// Override how this component paints itself
+	public void paintComponent(Graphics g){
+		g.setColor(Color.BLACK);
+		g.fillRect(0,0,1280,720);
 	
-	public void paintComponent (Graphics g)
-	{
-		super.paintComponent(g);
-		
-		if (printTile == true)
-		{
-			g.drawImage(image, intOreX, intOreY, null);
-			System.out.println("print");
-		}
-	}
-	
-	public void paintChildren (Graphics g)
-	{
-		
 	}
 
 	// Constructor
 	public AnimationPanel()
 	{
 		super();
-		
-		setBackground(Color.black);
-		
-		try
-		{
-			image = ImageIO.read(new File("OreTile.png"));
-		}
-		catch (IOException e)
-		{
-			
-		}
 	}
 }
