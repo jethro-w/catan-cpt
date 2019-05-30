92a4a34427f70192cde41737b7ea9b8a07d8dd47
jasonchan239 <50674864+jasonchan239@users.noreply.github.com>
2019-05-30 14:26:15 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/jethro-w/catan-cpt""
    This reverts commit cb111c014e73de47aa0e1447661caf2a1a04aab1.

--- ReplacementPanel.java
+++ ReplacementPanel.java
@@ -11,6 +11,7 @@ public class ReplacementPanel extends JPanel
 	BufferedImage background = null;
 	File file = null;
 	Font f = null;
+	boolean blnMainMenu = true;
 	boolean blnPlay = false;
 	boolean blnSettings = false;
 	boolean blnHelp = false;
@@ -21,59 +22,53 @@ public class ReplacementPanel extends JPanel
 		
 		g.setFont(f);
 		
-		if (blnPlay){
-			g.setColor(Color.RED);
-			g.drawString("Play",900,350);
-			g.setColor(Color.BLACK);
-			g.drawString("Settings",900,430);
-			g.drawString("Help",900,510);
-			g.drawString("Quit",900,590);
-		}else if (blnSettings)
-		{
-			g.setColor(Color.BLACK);
-			g.drawString("Play",900,350);
-		
-			g.setColor(Color.RED);
-			g.drawString("Settings",900,430);
-			g.setColor(Color.BLACK);
-			g.drawString("Help",900,510);
-			g.drawString("Quit",900,590);
+		if (blnMainMenu){
+			if (blnPlay){
+				g.setColor(Color.RED);
+				g.drawString("Play",900,350);
+				g.setColor(Color.BLACK);
+				g.drawString("Settings",900,430);
+				g.drawString("Help",900,510);
+				g.drawString("Quit",900,590);
+			}else if (blnSettings)
+			{
+				g.setColor(Color.BLACK);
+				g.drawString("Play",900,350);
+			
+				g.setColor(Color.RED);
+				g.drawString("Settings",900,430);
+				g.setColor(Color.BLACK);
+				g.drawString("Help",900,510);
+				g.drawString("Quit",900,590);
 
-		g.drawString("Play",1000,300);
-		
-		g.drawString("Settings",1000,400);
-		
-		g.drawString("Help",1000,500);
-	
-		}else if (blnHelp)
-		{	
-			g.setColor(Color.BLACK);
-			g.drawString("Play",900,350);
-			g.drawString("Settings",900,430);
-			g.setColor(Color.RED);
-			g.drawString("Help",900,510);
-			g.setColor(Color.BLACK);			
-			g.drawString("Quit",900,590);
+			}else if (blnHelp)
+			{	
+				g.setColor(Color.BLACK);
+				g.drawString("Play",900,350);
+				g.drawString("Settings",900,430);
+				g.setColor(Color.RED);
+				g.drawString("Help",900,510);
+				g.setColor(Color.BLACK);			
+				g.drawString("Quit",900,590);
+				
+			}else if (blnQuit)
+			{
+				g.setColor(Color.BLACK);
+				g.drawString("Play",900,350);
+				g.drawString("Settings",900,430);
+				g.drawString("Help",900,510);
+				g.setColor(Color.RED);
+				g.drawString("Quit",900,590);
+				
+			}else{
+				g.setColor(Color.BLACK);
+				g.drawString("Play",900,350);
+				g.drawString("Settings",900,430);
+				g.drawString("Help",900,510);
+				g.drawString("Quit",900,590);
 			
-		}else if (blnQuit)
-		{
-			g.setColor(Color.BLACK);
-			g.drawString("Play",900,350);
-			g.drawString("Settings",900,430);
-			g.drawString("Help",900,510);
-			g.setColor(Color.RED);
-			g.drawString("Quit",900,590);
-			
-		}else{
-			g.setColor(Color.BLACK);
-			g.drawRect(900,475,100,50);
-			g.drawString("Play",900,350);
-			g.drawString("Settings",900,430);
-			g.drawString("Help",900,510);
-			g.drawString("Quit",900,590);
-		
+			}
 		}
-
 
 	}
 	public ReplacementPanel()
@@ -84,7 +79,6 @@ public class ReplacementPanel extends JPanel
 		{
 			background = ImageIO.read(new File("catanmenu.jpg"));
 			f = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("ipixelu.ttf"))).deriveFont(Font.PLAIN,48);
-			
 		}catch(IOException e)
 		{
 		}catch(FontFormatException e)
